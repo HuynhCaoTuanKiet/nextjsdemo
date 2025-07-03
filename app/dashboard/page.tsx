786618884061d2c32d5cf885aad3aa60d71fde55
Kiet Huynh Cao Tuan <kiet.huynh2@orientsoftware.com>
2025-07-03 11:10:23 +0700

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// export default function Page() {
+//     return <p>Dashboard Page</p>;
+//   }
+
+const dashboardPage = () => {
+    return (
+        <p>Dashboard Page</p>
+    );
+};
+
+export default dashboardPage;
